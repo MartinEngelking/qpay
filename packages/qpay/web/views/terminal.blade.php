533ea9028e5d7c25e3ca880c64625dd4f3cb0f03
Martin Engelking <martin.engelking@gmail.com>
2016-02-26 00:31:38 -0500

    Added transaction list placeholder.

--- packages/qpay/web/views/terminal.blade.php
+++ packages/qpay/web/views/terminal.blade.php
@@ -5,4 +5,5 @@ $title = 'QPay Payment Terminal';
 @extends('web::layout')
 @section('content')
     @include('web::transaction-form')
+    @include('web::transaction-list')
 @endsection
---
+++ packages/qpay/web/views/transaction-list.php
@@ -0,0 +1,11 @@
+<div id="transactionListContainer">
+    <h3>Previous transactions:</h3>
+<table id="transactionList" class="table-striped">
+<thead>
+
+</thead>
+<tbody>
+
+</tbody>
+</table>
+</div>
