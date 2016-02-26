4c7a94642c85bbef5315a164e3d8dceb8c651741
Martin Engelking <martin.engelking@gmail.com>
2016-02-26 00:03:44 -0500

    Implemented listing of transactions.

--- packages/qpay/api/src/Http/Controllers/TransactionController.php
+++ packages/qpay/api/src/Http/Controllers/TransactionController.php
@@ -15,11 +15,12 @@ class TransactionController extends Controller
      */
     public function index()
     {
-        $status = 200;
-        $response = [
-            'status' => 'ok'
-        ];
-        return Response::json($response, $status);
+        try {
+            $transactions = $this->_getTransactions();
+            return $this->_transactionListResponse($transactions);
+        } catch (\Exception $e) {
+            return $this->_errorResponse($e->getMessage());
+        }
     }
 
     /**
@@ -35,5 +36,86 @@ class TransactionController extends Controller
             'status' => 'ok'
         ];
         return Response::json($response, $status);
+    }
+
+    /**
+     * Returns a list of all transactions
+     *
+     * @return mixed
+     */
+    private function _getTransactions()
+    {
+        return Transaction::orderBy('id', 'asc')->get()->all();
+    }
+
+    /**
+     * Returns a response with a list of transactions
+     *
+     * @param $transactions
+     * @return Response
+     */
+    private function _transactionListResponse($transactions)
+    {
+
+        $transactions = $this->_responsifyTransactions($transactions);
+
+        $response = [
+            'transactions' => $transactions,
+            'status' => 'ok'
+        ];
+
+        return Response::json($response, 500);
+
+    }
+
+    /**
+     * Returns an error response
+     *
+     * @param $message
+     * @return mixed
+     */
+    private function _errorResponse($message)
+    {
+        $response = [
+            'status' => 'error',
+            'error' => $message
+        ];
+
+        return Response::json($response, 500);
+
+    }
+
+    /**
+     * Turns an array of Transaction objects into something suitable for our API response.
+     *
+     * @param $transactions array
+     * @return array
+     */
+    private function _responsifyTransactions($transactions)
+    {
+        return array_map(function($transaction) {
+            return $this->_responsifyTransaction($transaction);
+        }, $transactions);
+    }
+
+    /**
+     * Returns an associative array describing a transaction.
+     * Used to compose the output of our API.
+     *
+     * @param $transaction \QPay\Core\Transaction
+     * @return array
+     */
+    protected function _responsifyTransaction($transaction)
+    {
+        return [
+            'id' => $transaction->id,
+            'amount' => $transaction->amount,
+            'merchant' => $transaction->merchant,
+            'address' => $transaction->address,
+            'city' => $transaction->geolocation->city,
+            'state' => $transaction->geolocation->state,
+            'zip' => $transaction->geolocation->zip,
+            'timestamp' => $transaction->timestamp
+        ];
     }
 }
