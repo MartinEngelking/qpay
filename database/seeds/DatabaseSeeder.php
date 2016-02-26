8dff96e643d84bd32dd60a905b54fd92bd89fa38
Martin Engelking <martin.engelking@gmail.com>
2016-02-25 23:34:20 -0500

    Add transaction seed.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    const TRANSACTION_CSV = 'transactions.csv';
+    const TRANSACTIONS  = 'transactions';
+
     /**
      * Run the database seeds.
      *
@@ -11,6 +14,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserTableSeeder::class);
+        $this->seedTransactions();  // todo: This should be defined in its own seeder class
     }
+
+    protected function seedTransactions()
+    {
+        $csv_path = __DIR__ . '/' . self::TRANSACTION_CSV;
+        $file = fopen($csv_path, 'r');
+        $columns = fgetcsv($file);
+
+        // For a small file, it's actually more efficient to read it all into memory and send it over to the DB
+        // in a single query. This approach handles arbitrarily large datasets well.
+        while($row = fgetcsv($file)) {
+            // Turn row into associative array
+            $transaction_attributes = array_combine($columns, $row);
+            DB::table(self::TRANSACTIONS)->insert($transaction_attributes);
+        }
+
+        fclose($file);
+    }
+
 }
